022e898f540abd86e253329ef671d1f3a67efc1f
Bioinfowangm <mengwang55@gmail.com>
2022-12-08 16:17:23 -0800

    Update checkorder.pl

--- Scripts/Additional_Codes/freebayes_scripts/checkorder.pl
+++ Scripts/Additional_Codes/freebayes_scripts/checkorder.pl
@@ -1,13 +1,4 @@
 #!/usr/bin/perl 
-#===========================================================================
-#
-#         FILE: checkorder.pl
-#        USAGE: perl checkorder.pl
-#
-#       AUTHOR: Wang Meng, mengwang55@gmail.com
-#      VERSION: 1.0
-#      CREATED: 07/23/2019 09:31:31
-#===========================================================================
 
 use strict;
 use warnings;
@@ -16,22 +7,24 @@ my $input  = $ARGV[0];
 my $output = $ARGV[1];
 open I_f, $input;
 open O_f, ">$output";
-my $line = `head -154 $input|tail -1`;
-my @row  = split /\t/, $line;
-if ( $row[-1] =~ /ormal/ ) {
+
+chomp(my @lines = <I_f>);
+
+my $header = grep {/^\#CHROM/} @lines;
+my @row  = split /\t/, $header;
+if ( $row[-1] =~ /Normal/i ) {
     system("cp $input $output");
 }
 else {
-    while (<I_f>) {
-        chomp;
-        my @row = split;
+    for my $l(@lines){
+        my @row = split /\t/,$l;
         if (/^##/) {
-            print O_f "$_\n";
+            print O_f "$l\n";
         }
         else {
             print O_f
-              join( "\t", @row[ 0 .. ( $#row - 2 ) ], $row[-1], $row[-2] ),
-              "\n";
+            join( "\t", @row[ 0 .. ( $#row - 2 ) ], $row[-1], $row[-2] ),
+            "\n";
         }
     }
 }
