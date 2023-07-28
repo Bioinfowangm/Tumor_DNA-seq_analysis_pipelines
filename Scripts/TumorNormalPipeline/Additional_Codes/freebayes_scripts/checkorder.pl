7fc7e2fa1ff703fd2ca2443508e24fa757fc9e28
Bioinfowangm <mengwang55@gmail.com>
2023-07-27 18:53:26 -0700

    Update checkorder.pl

--- Scripts/TumorNormalPipeline/Additional_Codes/freebayes_scripts/checkorder.pl
+++ Scripts/TumorNormalPipeline/Additional_Codes/freebayes_scripts/checkorder.pl
@@ -3,29 +3,29 @@
 use strict;
 use warnings;
 
-my $input  = $ARGV[0];
-my $output = $ARGV[1];
+my $input    = $ARGV[0];
+my $output   = $ARGV[1];
 my $ctl_name = $ARGV[2];
 open I_f, $input;
 open O_f, ">$output";
 
-chomp(my @lines = <I_f>);
+chomp( my @lines = <I_f> );
 
-my $header = grep {/^\#CHROM/} @lines;
-my @row  = split /\t/, $header;
+my $header = grep { /^\#CHROM/ } @lines;
+my @row    = split /\t/, $header;
 if ( $row[-1] eq $ctl_name ) {
     system("cp $input $output");
 }
 else {
-    for my $l(@lines){
-        my @row = split /\t/,$l;
-        if (/^##/) {
+    for my $l (@lines) {
+        my @row = split /\t/, $l;
+        if ( $l =~ /^##/ ) {
             print O_f "$l\n";
         }
         else {
             print O_f
-            join( "\t", @row[ 0 .. ( $#row - 2 ) ], $row[-1], $row[-2] ),
-            "\n";
+              join( "\t", @row[ 0 .. ( $#row - 2 ) ], $row[-1], $row[-2] ),
+              "\n";
         }
     }
 }
